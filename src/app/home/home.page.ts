cde77310ef354b66c920a58d5ee23fafddc541da
Anirudh <anirudhgopal55@gmail.com>
2023-09-09 14:01:54 -0400

    Init Design of Home Page with Mocked data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Service } from '../interfaces/common-interfaces';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public servicesList: Service[] = [
+    {
+      id: 'radarr',
+      label: 'Radarr',
+      shortDesc: 'Movie Management',
+      desc: 'Radarr is a movie collection manager for Usenet and BitTorrent users.'
+    },
+    {
+      id: 'sonarr',
+      label: 'Sonarr',
+      shortDesc: 'TV Show Management',
+      desc: 'Sonarr is an internet PVR for Usenet and Torrents.'
+    },
+    {
+      id: 'bazarr',
+      label: 'Bazarr',
+      shortDesc: 'Subtitle Management',
+      desc: 'Bazarr is a companion application to Sonarr and Radarr that manages and downloads subtitles based on your requirements.'
+    },
+    {
+      id: 'transmission',
+      label: 'Transmission',
+      shortDesc: 'Torrent Management',
+      desc: 'Transmission is a BitTorrent client which features a variety of user interfaces on top of a cross-platform back-end.'
+    }
+  ];
 
   constructor() {}
 
---
+++ src/app/interfaces/common-interfaces.ts
@@ -0,0 +1,6 @@
+export interface Service {
+    id: string;
+    label: string;
+    desc: string;
+    shortDesc: string;
+}
